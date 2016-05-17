527b77e53cd1dc96d74b571fe502bdbdf5551fc5
Benjamin Cheng <ben@bcheng.cf>
2016-05-16 20:56:32 -0400

    Fix cutting off ending tags

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -7,7 +7,7 @@
                 <div class="post-content">
                     <span class="title">{{$post->title}}</span>
                     <span class="time">{{$post->created_at}}</span>
-                    <span class="excerpt">{!!str_limit(Markdown::convertToHtml($post->body), $limit = 200, $end='...')!!}</span>
+                    <span class="excerpt">{!!Markdown::convertToHTML(str_limit($post->body, $limit = 200, $end='...'))!!}</span>
                 </div>
             </a>
         @endforeach
